72bf0ae036fd01539ecb04f03119e6e4c92a5ff9
Jinso Raj <jinsoraj2000@gmail.com>
2024-06-16 00:23:43 +0530

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## IFIApp
+
+Flutter Frappe App for Insight for innovation
+
+#### License
+
+MIT
---
+++ ifiapp/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "IFIApp",
+			"type": "module",
+			"label": _("IFIApp")
+		}
+	]
