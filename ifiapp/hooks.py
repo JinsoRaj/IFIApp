80e7619cee05d89528eb54b79a1987c7bc7f3d1d
JinsoRaj <jinsoraj2000@gmail.com>
2024-07-28 00:10:01 +0530

    marks and levels, topics, modules

--- ifiapp/hooks.py
+++ ifiapp/hooks.py
@@ -214,7 +214,7 @@ app_license = "MIT"
 # 	"ifiapp.auth.validate"
 # ]
 
-fixtures = [{
+fixtures = ["Topic", "IFIModule",{
   'dt': 'Role', 'filters': {'name': ('in', ('Volunteer','Coordinator','Quality Assurance','ifiuser'))},
   'dt': 'Role Profile', 'filters': {'name': ('in', ('ifi'))},
   'dt': 'Workflow', 'filters': {'name': ('in', ('UserApproval'))},
