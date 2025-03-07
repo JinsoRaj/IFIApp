83a01b891723cf49ff89a8539e52dec1b277da57
Jinso Raj <jinsoraj2000@gmail.com>
2025-03-08 00:24:06 +0530

    deleted tbeval

--- ifiapp/ifiapp/doctype/tbevaluation/test_tbevaluation.py
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024, JinsoRaj and Contributors
-# See license.txt
-
-# import frappe
-from frappe.tests.utils import FrappeTestCase
-
-
-class TestTBEvaluation(FrappeTestCase):
-	pass
