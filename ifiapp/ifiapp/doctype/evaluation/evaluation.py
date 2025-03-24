b037be2c7feef51b4974b94fde26d61b9de293d8
Jinso Raj <jinsoraj2000@gmail.com>
2025-03-24 14:52:20 +0530

    fetch profile pic also in evaluation

--- ifiapp/ifiapp/doctype/evaluation/evaluation.py
+++ ifiapp/ifiapp/doctype/evaluation/evaluation.py
@@ -9,7 +9,9 @@ class Evaluation(Document):
 	def before_save(self):
 		ifi_id = self.ifi_id
 		app_user = frappe.db.get_value("AppUser", {"ifi_id": ifi_id},"name")
+		app_user_profile = frappe.db.get_value("AppUser", {"ifi_id": ifi_id},"profile_pic")
 		self.volunteer = app_user
+		self.profile_pic = app_user_profile
 		if self.volunteer == None:
 			frappe.throw("No such volunteer IFI ID")
                   
