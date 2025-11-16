444545ef407b559d5332a9f69af5f202667ccc4c
Jinso Raj <jinsoraj2000@gmail.com>
2025-11-16 21:31:21 +0530

    commented auto time calc

--- ifiapp/ifiapp/doctype/classsession/classsession.py
+++ ifiapp/ifiapp/doctype/classsession/classsession.py
@@ -9,6 +9,7 @@ import math
 
 class ClassSession(Document):
    pass
+	#commenting this beacuse we agreed to take the hours from the volunteer itself instead of auto calculating the hours.
 	# def before_save(self):
 	# 	if self.session_start_time and self.session_end_time:
 	# 		start = get_datetime(self.session_start_time)
